0581cc19ab4abb2ff00fc2b28e5ab3f548773dc4
Nate Claussen <natevan.email@gmail.com>
2021-02-18 10:11:59 -0700

    Testing Changes

--- resume.js
+++ resume.js
@@ -11,7 +11,7 @@ function toggleSlide(selectedCard) {
 }
 
 function toggleAllSlides() {
-
+    //This is a comment to see if my snippet actually changes and updates! :O
 }
 
 $(document).ready(function() {
